09cb4e9396b0508bdbd41ffaaa8dee9a47e45440
Spyros Smparounis <nflare@gmail.com>
2012-08-01 06:40:21 +0300

    Added two accessors to KDTreeCell (BoundingBox and center point)

--- src/kdtree/KDTreeCell.java
+++ src/kdtree/KDTreeCell.java
@@ -128,7 +128,8 @@ public class KDTreeCell {
         }
     }
 
-    
+    public BoundingBox getBoundingBox() { return this.cell; }
+    public Float getCenter() { return this.cell.getCenter(); }
     
     public Integer getMaxDepth() {
         if (this.children == null) return this.depth;
