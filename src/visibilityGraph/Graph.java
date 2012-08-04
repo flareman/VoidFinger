dd3606d0651cb4f311bf056951c68884c3162f47
george <george@george-Aspire-2920>
2012-08-04 04:34:21 +0300

    Implemented getNeighboors method given a certain node id. Implemented basic Dijkstra's Shortest Path Algorithm for given start and end nodes. Placeholder for calculating set of all minimal path costs for all pairs in the visibility graph.

--- src/visibilityGraph/Graph.java
+++ src/visibilityGraph/Graph.java
@@ -103,4 +103,65 @@ public class Graph {
             }
         }
     }
+    
+    private ArrayList<Integer> getNeighboors(int node){
+        ArrayList<Integer> neighboors = new ArrayList<Integer>();
+        for(GraphEdge e : this.edges){
+            if(e.getNodes()[0] == node){
+                neighboors.add(e.getNodes()[1]);
+            }
+            if(e.getNodes()[1] == node){
+                neighboors.add(e.getNodes()[0]);
+            }
+        }
+        return neighboors;
+    }
+    
+    private Float calculateShortestPathDijkstra(int start, int end){
+        Float[] tentative = new Float[nodes.size()];
+        tentative[start] = 0.0f;
+        for(int i=0;i<nodes.size();i++){
+            if(i!=start)
+                tentative[i] = Float.POSITIVE_INFINITY;
+        }
+        int current = start;
+        ArrayList<Integer> visited = new ArrayList<Integer>();
+        ArrayList<Integer> neighboors;
+        while(!visited.contains(end)){
+            neighboors = getNeighboors(current);
+            for(Integer i : neighboors){
+                Float weight = 0.0f;
+                for(GraphEdge e : this.edges){
+                    Integer[] nds = e.getNodes();
+                    if(nds[0] == current && nds[1] == i)
+                        weight = e.getWeight();
+                    if(nds[1] == current && nds[0] == i)
+                        weight = e.getWeight();
+                }
+                tentative[i] = tentative[current] + weight;
+            }
+            visited.add(current);
+            Float minDist = Float.POSITIVE_INFINITY;
+            int next = -1;
+            for(int i=0;i<tentative.length;i++){
+                if((!visited.contains(i)) && (tentative[i] < minDist)){
+                    minDist = tentative[i];
+                    next = i;
+                }
+            }
+            if(next == -1)
+                break;
+            current = next;
+        }
+        return tentative[end];
+    }
+    
+    public ArrayList<Float> getShortestPathCosts(){
+        ArrayList<Float> costs = new ArrayList<Float>();
+        
+        
+        
+        return costs;
+    }
+    
 }
