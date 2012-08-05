de6937c549a0b6190e2be7a8f29f020dcd82d625
Spyros Smparounis <nflare@gmail.com>
2012-08-06 00:19:32 +0300

    Updated main to proceed up to the inner distance creation

--- src/voidfinger/VoidFinger.java
+++ src/voidfinger/VoidFinger.java
@@ -4,15 +4,19 @@ import filter.FCEException;
 import filter.FilterClusterEngine;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import kdtree.KDTree;
 import kdtree.KDTreeException;
 import octree.Octree;
 import octree.OctreeException;
+import visibilityGraph.Graph;
+import visibilityGraph.GraphException;
 
 public class VoidFinger {
     private Octree molecule = null;
     private KDTree kdtree = null;
     private FilterClusterEngine fce = null;
+    private Graph graph = null;
     
     public VoidFinger(String filename) {
         try {
@@ -24,8 +28,15 @@ public class VoidFinger {
             this.molecule = Octree.parseFromFile(filename);
             this.kdtree = new KDTree(this.molecule);
             this.fce = new FilterClusterEngine(this.kdtree, 500);
+            System.out.print("Filtering kd-tree...");
             this.fce.performClustering();
-            System.out.println("Calculated "+this.fce.getClusterCenters().size()+" centers.");
+            this.graph = new Graph(this.fce.getClusterCenters(), this.molecule);
+            System.out.print("Building visibilty graph... ");
+            this.graph.buildVisibilityGraph();
+            System.out.println("done");
+            System.out.print("Calculating inner distances... ");
+            ArrayList<Float> result = this.graph.getInnerDistances();
+            System.out.println("done");
         } catch (FileNotFoundException fe) {
             System.out.println(fe.getLocalizedMessage());
             this.molecule = null;
@@ -40,6 +51,9 @@ public class VoidFinger {
             this.molecule = null;
         } catch (FCEException fcee) {
             System.out.println(fcee.getLocalizedMessage());
+            this.molecule = null;
+        } catch (GraphException gre) {
+            System.out.println(gre.getLocalizedMessage());
             this.molecule = null;
         }
     }
