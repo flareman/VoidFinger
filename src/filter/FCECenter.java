098c5b7dacd7cca7d1419d7015813cdbeae6e54b
Spyros Smparounis <nflare@gmail.com>
2012-08-05 20:31:45 +0300

    Added convergence checks and methods

--- src/filter/FCECenter.java
+++ src/filter/FCECenter.java
@@ -7,6 +7,7 @@ public class FCECenter {
     private Point center = null;
     private Point sum = null;
     private Integer count = 0;
+    private Point previous = null;
     
     public FCECenter(Point p) throws FCEException {
         if (p == null) throw new FCEException("All cluster centers must be initialized with non-null points");
@@ -35,10 +36,21 @@ public class FCECenter {
     }
     
     public void updateCenter() {
+        if (this.count == 0) return;
+        this.previous = this.center;
         this.center = this.sum.scaledPoint(1.0f/this.count);
         try {
             this.sum = Point.zeroPoint(this.center.getDimensions());
             this.count = 0;
         } catch (GeometryException ge) {}
+    }
+    
+    public Boolean hasConverged(Float threshold) {
+        if (this.previous == null) return false;
+        try {
+            Float dist = this.previous.euclideanDistanceFrom(this.center);
+            if (dist <= threshold) return true;
+            return false;
+        } catch (GeometryException ge) { return false;}
     }
 }
